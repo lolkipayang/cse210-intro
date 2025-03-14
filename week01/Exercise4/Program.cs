e909088b9b1247b3ef77c9bd339090ea0f7949c5
lolkipayang <llolkipayangi@byupathway.edu>
2025-03-14 18:54:26 +0300

    Update Program.cs
    Core requirement 2

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -30,5 +30,8 @@ class Program
         }
 
         Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
     }
 }
