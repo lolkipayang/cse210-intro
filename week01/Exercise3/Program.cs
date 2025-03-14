b658ba846de38e41c6802f8f3404780c179968c4
lolkipayang <llolkipayangi@byupathway.edu>
2025-03-14 16:06:50 +0300

    Update Program.cs
    Core requirement 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number?");
+        string magic = Console.ReadLine();
+        Console.Write("What is your guess?");
+        string guess = Console.ReadLine();
+        int magicNum = int.Parse(magic);
+        int guessNum = int.Parse(guess);
+        
+        if (guessNum > magicNum)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guessNum == magicNum)
+        {
+            Console.WriteLine("You guessed it");
+        }
+        else
+        {
+            Console.WriteLine("Higher");
+        }
     }
 }
