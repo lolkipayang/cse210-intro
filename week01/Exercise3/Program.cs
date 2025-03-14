d266ecc3faaafe07d140f9029986585c054ef540
lolkipayang <llolkipayangi@byupathway.edu>
2025-03-14 16:46:21 +0300

    Update Program.cs
    Core requirement 2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,24 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number?");
-        string magic = Console.ReadLine();
-        Console.Write("What is your guess?");
-        string guess = Console.ReadLine();
-        int magicNum = int.Parse(magic);
-        int guessNum = int.Parse(guess);
         
-        if (guessNum > magicNum)
+        Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int magicNum = int.Parse(magic);
+        int guessNum = 0;
+        do 
         {
-            Console.WriteLine("Lower");
+
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNum = int.Parse(guess);
+
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
-        else if (guessNum == magicNum)
-        {
-            Console.WriteLine("You guessed it");
-        }
-        else
-        {
-            Console.WriteLine("Higher");
-        }
+
+        while (guessNum != magicNum);
     }
 }
