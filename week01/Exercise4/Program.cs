4ec057b76297a5fc8c5bf3bb35dbb28797f8da90
lolkipayang <llolkipayangi@byupathway.edu>
2025-03-14 18:52:55 +0300

    Update Program.cs
    Core requirement 1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+         List<int> numbers = new List<int>();
+        
+    
+        int num;
+        do
+        {
+            Console.Write("Enter a number greater or less than zero, or zero to quit: ");
+            
+            string userInput = Console.ReadLine();
+            num = int.Parse(userInput);
+            
+    
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+        }
+        while (num != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
