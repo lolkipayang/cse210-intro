cd665c6a66ec2b9bfec1a6db59ed835645e19d9e
lolkipayang <llolkipayangi@byupathway.edu>
2025-03-14 15:11:58 +0300

    Update Program.cs
    Core requirement 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+         if (number >= 90)
+        {
+            Console.Write("Your grade is A");
+            string letter = Console.ReadLine();
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }
